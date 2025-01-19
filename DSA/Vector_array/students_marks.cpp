1e17b03d2966a03574240ac494d849107b336901
johniton <johnitonmas8@gmail.com>
2025-01-19 21:28:30 +0530

    Added new functions

--- DSA/Vector_array/students_marks.cpp
+++ DSA/Vector_array/students_marks.cpp
@@ -1,15 +1,15 @@
+#include <cmath>
 #include<iostream>
 #include<vector>
 using namespace std;
 
-/*void addstudent();  DONE
-void updateScore();
-void avgScore();
-void highestAvg();
-void display();      DONE
-*/
+
+void highestAvgScore(vector<vector<int>>& vec);
+void avgScoreStudent(vector<vector<int>>& vec);
 void display(vector<vector<int>> vec);
+void avgScoreClass(vector<vector<int>> vec);
 void addStudent(vector<vector<int>>& vec);
+void updateScore(vector<vector<int>>& vec);
 int main(){
 	int rows,columns;
 	cout<<"\n Enter number of Students and Subjects:";
@@ -29,24 +29,25 @@ int main(){
 	int choice;
 	do
 	{
-	cout<<"\n Choose \n 1] Add a student \n 2] Update the Student's Score \n 3] Calculate the avg of the student \n 4] Highest marks \n 5] Display Students \n 6] Exit \n ";
+	cout<<"\n Choose \n 1] Add a student \n 2] Update the Student's Score \n 3] Calculate the avg of the student \n 4] Highest avg \n 5]Display avg of class \n 6] Display Students \n 7] Exit \n ";
 	cin>>choice;
-
 	switch(choice){
 		case 1:addStudent(vec); break;
-		case 2:break;
-		case 3:break;
-		case 4:break;
-		case 5:display(vec);break;
+		case 2:updateScore(vec); break;
+		case 3:avgScoreStudent(vec); break;
+		case 4:highestAvgScore(vec);break;
+		case 5:avgScoreClass(vec);break;
+		case 6:display(vec);break;
 		default:break;
 	}
 
-	}while(choice!=6);
+	}while(choice!=7);
 
 }
 
 void display(vector<vector<int>> vec){
 	for(int i=0;i<vec.size();i++){
+		cout<<"\n Student"<<i+1<<":";
 		for(int j=0;j<vec[i].size();j++){
 			cout<<vec[i][j]<<" ";
 		}
@@ -66,8 +67,56 @@ void addStudent(vector<vector<int>>& vec){
 	}	
 
 }
+void updateScore(vector<vector<int>>& vec){
+	cout<<"\n Enter student Number whose score you want to update:";
+	int no;
+	cin>>no;
+	cout<<"\n Enter marks of "<<vec[no-1].size()<<" Subjects:";
+	for(int j=0;j<vec[no -1].size();j++){
+			cin>>vec[no-1][j];
+	}
+}
 
 
+void avgScoreClass(vector<vector<int>> vec){
+	int counter=0;
+	float sum=0;
+	for(int i=0;i<vec.size();i++){
+		counter++;
+		for(int j=0;j<vec[i].size();j++)
+			sum+=vec[i][j];
+	}
 
+	cout<<"\n Average of class = "<<sum/counter;
+	
+
+}
+
+void avgScoreStudent(vector<vector<int>>& vec){
+ 	cout<<"\n Enter Student number:";
+	int no;
+	float sum=0;
+	cin>>no;
+		for(int j=0;j<vec[no -1].size();j++){
+			sum+=vec[no-1][j];
+	}
+	cout<<"\n Average of student = "<<sum/vec[no-1].size();
+	}
+void highestAvgScore(vector<vector<int>>& vec){
+	float max=0,avg,sum=0;
+	int index,j;
+		for(int i=0;i<vec.size();i++){
+			sum=0;
+			for( j=0;j<vec[i].size();j++){
+				sum+=vec[i][j];
+			}
+			avg=sum/vec[i].size();
+			if (max<avg){
+				max=avg;
+				index=i;
+			}
+		}
+		cout<<"\n Highest avg is of student "<<index+1<<"which is "<<max;
+}
 
 
